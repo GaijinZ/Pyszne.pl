8d227f402343cc1bfa44327fae96faf0913d65bd
GaijinZ <tomasz.mostowy@gmail.com>
2021-10-21 20:38:03 +0200

    last name required to register

--- yummy/accounts/models.py
+++ yummy/accounts/models.py
@@ -4,19 +4,22 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 class MyUserManager(BaseUserManager):
 
-    def create_user(self, email, password=None):
-
+    def create_user(self, email, last_name, password=None):
         if not email:
             raise ValueError('Users must have email address')
 
-        user = self.model(email.self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email),
+                          last_name=last_name)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
-
-        user = self.create_user(email, password=password)
+    def create_superuser(self, email, last_name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            last_name=last_name,
+            password=password,
+        )
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -34,7 +37,7 @@ class User(AbstractBaseUser):
     objects = MyUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['last_name']
 
     def has_perm(self, perm, obj=None):
         return True
