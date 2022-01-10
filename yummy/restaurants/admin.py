770d4e4c16d0b4e9f3f32c63cf71fa549e44fb0d
GaijinZ <tomasz.mostowy@gmail.com>
2022-01-10 22:30:56 +0100

    string methods models.py

--- yummy/restaurants/admin.py
+++ yummy/restaurants/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Restaurant, TypeOfRestaurant, Delivery
+from .models import Restaurant, TypeOfRestaurant, Delivery, Address
 
 
 admin.site.register(Restaurant)
 admin.site.register(TypeOfRestaurant)
 admin.site.register(Delivery)
+admin.site.register(Address)
--- yummy/restaurants/models.py
+++ yummy/restaurants/models.py
@@ -3,13 +3,19 @@ from django.db import models
 
 class Address(models.Model):
     city = models.CharField(max_length=250)
-    post_code = models.IntegerField()
+    post_code = models.CharField(max_length=10)
     street_name = models.CharField(max_length=250)
     building_number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.city} {self.post_code}, {self.street_name}'
 
 
 class TypeOfRestaurant(models.Model):
     type_of_restaurant = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.type_of_restaurant}'
 
 
 class Delivery(models.Model):
@@ -17,10 +23,16 @@ class Delivery(models.Model):
     price_to_deliver = models.IntegerField(null=True, blank=True)
     time_to_deliver = models.FloatField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.delivery_option}'
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     restaurant_type = models.ManyToManyField(TypeOfRestaurant)
     delivery = models.OneToOneField(Delivery, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='restaurant_picture')
+    picture = models.ImageField(upload_to='restaurant_picture', blank=True)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
